61dcb82e9e7137e2a4611c59bc1629d64ee7c0f3
kanaishi_hitoshi <sapporokyorokyoro@gmail.com>
2018-10-20 15:40:37 +0900

    Lesson6課題＆発展課題
    うまく削除されてない気がします。
    アドバイス宜しくお願いします。

---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour {
+    private GameObject unitychan;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+        //Debug.Log(this.unitychan.transform.position.z);
+        if (this.unitychan.transform.position.z - 5 >= this.transform.position.z)
+        {
+            //
+            Destroy(gameObject);
+            Debug.Log("削除成功");
+            //Debug.Log(this.unitychan.transform.position.z);
+        }
+
+
+    }
+}
